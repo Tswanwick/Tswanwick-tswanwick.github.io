58349593d6cb6be9c1d59648ac33b877c5ed96ad
Tessa <tswanwick@rebathmi.com>
2026-05-22 12:58:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const { MongoClient, ObjectId } = require("mongodb");
-require("dotenv").config();
+
 
 const app = express();
 app.use(cors({
